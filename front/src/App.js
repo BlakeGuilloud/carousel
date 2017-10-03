aabbd64926fca175b54ba65aae68d88429ce0ab2
Blake Guilloud <blake.guilloud@gmail.com>
2017-10-02 22:02:03 -0400

    React app.

---
+++ front/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as Actions from './actions';
+
+class App extends Component {
+  handleUploadFile = (event) => {
+    const reader = new FileReader();
+    const file = event.target.files[0];
+
+    reader.addEventListener('load', () => {
+      const fileData = reader.result;
+
+      Actions.uploadFile(fileData);
+
+    }, false);
+
+    reader.readAsDataURL(file);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <input type="file" onChange={this.handleUploadFile} />
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ front/src/actions.js
@@ -0,0 +1,5 @@
+// import axios from 'axios';
+
+export function uploadFile(file) {
+  console.log('woo woo woo', file);
+}
